2bc2c64d92a6de9cc3a593921d7b1165b4286fc7
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-06-01 14:04:38 +0200

    Gallery2: Replace hamburger menu with bottom bar
    Change-Id: I20caa58fe11ee6f4148cb0bcb0ccbcdcf3a9612e
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- src/com/android/gallery3d/app/PhotoPage.java
+++ src/com/android/gallery3d/app/PhotoPage.java
@@ -1021,6 +1021,7 @@ public abstract class PhotoPage extends ActivityState implements
     @Override
     protected void onBackPressed() {
         showBars();
+        ((GalleryActivity)mActivity).toggleNavBar(true);
         if (mShowDetails) {
             hideDetails();
         } else if (mAppBridge == null || !switchWithCaptureAnimation(-1)) {
@@ -1039,10 +1040,7 @@ public abstract class PhotoPage extends ActivityState implements
                 mActionBar.setBackGroundDefault();
                 int count = mActivity.getStateManager().getStateCount();
                 if (mIsFromVideoScreen || count == 1 || mIsFromTimelineScreen) {
-                    mActivity.getToolbar().setNavigationContentDescription(
-                            "drawer");
-                    mActivity.getToolbar().setNavigationIcon(R.drawable.drawer);
-                    ((GalleryActivity)mActivity).toggleNavDrawer(true);
+                    ((GalleryActivity)mActivity).toggleNavBar(true);
                     if (mModel instanceof PhotoDataAdapter) {
                         ((PhotoDataAdapter) mModel).setDataListener(null);
                     }
@@ -1663,12 +1661,11 @@ public abstract class PhotoPage extends ActivityState implements
         }
 
         mActionBar.setDisplayOptions(
-                ((mSecureAlbum == null) && (mSetPathString != null)), true);
+                (false && (mSetPathString != null)), true);
         mActionBar.addOnMenuVisibilityListener(mMenuVisibilityListener);
         refreshBottomControlsWhenReady();
         if (((mSecureAlbum == null) && (mSetPathString != null))) {
-            mActivity.getToolbar().setNavigationContentDescription("back");
-            mActivity.getToolbar().setNavigationIcon(R.drawable.back);
+            ((GalleryActivity)mActivity).toggleNavBar(false);
         }
         // if (mShowSpinner && mPhotoView.getFilmMode()) {
         // mActionBar.enableAlbumModeMenu(
